ae0c3fc5264cb93273ea0908d1430e9121d9d1f2
Rakib-Ahmed01 <rakibahmed34360@gmail.com>
2022-09-02 23:07:09 +0600

    main section done

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-console.log("Bismillah,,,");
+console.log("Bismillah...");
+
+const categories = document.getElementById("categories");
+
+const loadCategories = async () => {
+  const res = await fetch(
+    "https://openapi.programming-hero.com/api/news/categories"
+  );
+  const data = await res.json();
+  displayCategories(data);
+};
+
+loadCategories();
+
+const displayCategories = (data) => {
+  data = data.data.news_category;
+  data.forEach((category) => {
+    const li = document.createElement("li");
+    li.innerHTML = `
+        <li onclick=('${category.category_id}')>${category.category_name}</li>
+    `;
+    categories.appendChild(li);
+  });
+};
